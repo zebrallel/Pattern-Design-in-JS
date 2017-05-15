08b45d9f1ccf7f215f4b793dbf2642d8102d0954
xuejian.xu <xuejian.xu@qunar.com>
2017-05-15 11:53:10 +0800

    .

--- proxy.js
+++ proxy.js
@@ -4,3 +4,27 @@
  * @date: 2017/5/14.
  */
 
+//演唱会
+var Concert = function(){}
+//公司
+var Company = {
+    askforconcert: function(target){
+        var concert = new Concert();
+        target.openconcert(concert )
+    }
+
+}
+//明星
+var star = {
+    openconcert: function(concert){
+        console.log("明星同意开一场演唱会")
+    }
+}
+//经纪人代理
+var agent = {
+    openconcert: function(concert){
+        star.openconcert(concert)
+    }
+}
+//执行
+Company.askforconcert(agent);  //=>明星同意开一场演唱会
